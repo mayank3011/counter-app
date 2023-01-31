3b418d230834a37921df9f65148968f7f3128121
Mayank Rajput <mayankrajpu3012@gmail.com>
2023-01-31 19:27:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react'
+import bootstrap from 'bootstrap'
+export default function Counter(){
+  const [count, setCount]=useState(0);
+  const handleIncrement=()=>(
+    setCount(count+1)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  )
+  const handleDecrement=()=>(
+    setCount(count-1)
+  )
+  const handleReset=()=>(
+    setCount(0)
+  )
+
+  return(
+    <div>
+      <div className='d-flex justify-content-center align-items-center' style={{height: "50vh"}}>
+      <div>
+        <h1 className="text-center " style={{fontSize: "70px"}}>{count}</h1>
+        <div className="d-flex">
+          <button onClick={handleIncrement}className='btn btn-success me-3'>Increment</button>
+          <button onClick={handleReset} classname='btn btn-info me-3'>Reset</button>
+          <button onClick={handleDecrement}className='btn btn-danger me-3'>Decrement</button>
+        </div>
+      </div>
     </div>
-  );
+    <div>
+      <p className="text-center">Name: Mayank Rajput    Reg no:12011597</p>
+    </div>
+    </div>
+  )
 }
-
-export default App;
